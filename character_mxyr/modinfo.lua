323a55c8d5fe32c6f068cac3e61d50407783fd19
lunzard <60387314+lunzard@users.noreply.github.com>
2021-01-02 16:35:58 +0800

    Revert all

--- character_mxyr/modinfo.lua
+++ character_mxyr/modinfo.lua
@@ -1,7 +1,7 @@
 -- This information tells other players more about the mod
-name = "MXYR"
-description = "A mxyr character for Don't Starve Together."
-author = "lunzard"
+name = "Extended Sample Character"
+description = "A character template for Don't Starve Together."
+author = "Dragon Wolf Leo"
 version = "1.2.6" -- This is the version of the template. Change it to your own number.
 
 -- This is the URL name of the mod's thread on the forum; the part after the ? and before the first & in the url
--- character_mxyr/modmain.lua
+++ character_mxyr/modmain.lua
@@ -1,58 +1,58 @@
 PrefabFiles = {
-	"mxyr",
-	"mxyr_none",
+	"esctemplate",
+	"esctemplate_none",
 }
 
 Assets = {
-    Asset( "IMAGE", "images/saveslot_portraits/mxyr.tex" ),
-    Asset( "ATLAS", "images/saveslot_portraits/mxyr.xml" ),
+    Asset( "IMAGE", "images/saveslot_portraits/esctemplate.tex" ),
+    Asset( "ATLAS", "images/saveslot_portraits/esctemplate.xml" ),
 
-    Asset( "IMAGE", "images/selectscreen_portraits/mxyr.tex" ),
-    Asset( "ATLAS", "images/selectscreen_portraits/mxyr.xml" ),
+    Asset( "IMAGE", "images/selectscreen_portraits/esctemplate.tex" ),
+    Asset( "ATLAS", "images/selectscreen_portraits/esctemplate.xml" ),
 	
-    Asset( "IMAGE", "images/selectscreen_portraits/mxyr_silho.tex" ),
-    Asset( "ATLAS", "images/selectscreen_portraits/mxyr_silho.xml" ),
+    Asset( "IMAGE", "images/selectscreen_portraits/esctemplate_silho.tex" ),
+    Asset( "ATLAS", "images/selectscreen_portraits/esctemplate_silho.xml" ),
 
-    Asset( "IMAGE", "bigportraits/mxyr.tex" ),
-    Asset( "ATLAS", "bigportraits/mxyr.xml" ),
+    Asset( "IMAGE", "bigportraits/esctemplate.tex" ),
+    Asset( "ATLAS", "bigportraits/esctemplate.xml" ),
 	
-	Asset( "IMAGE", "images/map_icons/mxyr.tex" ),
-	Asset( "ATLAS", "images/map_icons/mxyr.xml" ),
+	Asset( "IMAGE", "images/map_icons/esctemplate.tex" ),
+	Asset( "ATLAS", "images/map_icons/esctemplate.xml" ),
 	
-	Asset( "IMAGE", "images/avatars/avatar_mxyr.tex" ),
-    Asset( "ATLAS", "images/avatars/avatar_mxyr.xml" ),
+	Asset( "IMAGE", "images/avatars/avatar_esctemplate.tex" ),
+    Asset( "ATLAS", "images/avatars/avatar_esctemplate.xml" ),
 	
-	Asset( "IMAGE", "images/avatars/avatar_ghost_mxyr.tex" ),
-    Asset( "ATLAS", "images/avatars/avatar_ghost_mxyr.xml" ),
+	Asset( "IMAGE", "images/avatars/avatar_ghost_esctemplate.tex" ),
+    Asset( "ATLAS", "images/avatars/avatar_ghost_esctemplate.xml" ),
 	
-	Asset( "IMAGE", "images/avatars/self_inspect_mxyr.tex" ),
-    Asset( "ATLAS", "images/avatars/self_inspect_mxyr.xml" ),
+	Asset( "IMAGE", "images/avatars/self_inspect_esctemplate.tex" ),
+    Asset( "ATLAS", "images/avatars/self_inspect_esctemplate.xml" ),
 	
-	Asset( "IMAGE", "images/names_mxyr.tex" ),
-    Asset( "ATLAS", "images/names_mxyr.xml" ),
+	Asset( "IMAGE", "images/names_esctemplate.tex" ),
+    Asset( "ATLAS", "images/names_esctemplate.xml" ),
 	
-	Asset( "IMAGE", "images/names_gold_mxyr.tex" ),
-    Asset( "ATLAS", "images/names_gold_mxyr.xml" ),
+	Asset( "IMAGE", "images/names_gold_esctemplate.tex" ),
+    Asset( "ATLAS", "images/names_gold_esctemplate.xml" ),
 }
 
-AddMinimapAtlas("images/map_icons/mxyr.xml")
+AddMinimapAtlas("images/map_icons/esctemplate.xml")
 
 local require = GLOBAL.require
 local STRINGS = GLOBAL.STRINGS
 
 -- The character select screen lines
-STRINGS.CHARACTER_TITLES.mxyr = "The toughest Character"
-STRINGS.CHARACTER_NAMES.mxyr = "mxyr"
-STRINGS.CHARACTER_DESCRIPTIONS.mxyr = "*Coffee addiction\n*Sleep everywhere\n*Love Coke"
-STRINGS.CHARACTER_QUOTES.mxyr = "\"Britian is the Frog City!\""
-STRINGS.CHARACTER_SURVIVABILITY.mxyr = "Slim"
+STRINGS.CHARACTER_TITLES.esctemplate = "The Sample Character"
+STRINGS.CHARACTER_NAMES.esctemplate = "Esc"
+STRINGS.CHARACTER_DESCRIPTIONS.esctemplate = "*Perk 1\n*Perk 2\n*Perk 3"
+STRINGS.CHARACTER_QUOTES.esctemplate = "\"Quote\""
+STRINGS.CHARACTER_SURVIVABILITY.esctemplate = "Slim"
 
 -- Custom speech strings
-STRINGS.CHARACTERS.MXYR = require "speech_mxyr"
+STRINGS.CHARACTERS.ESCTEMPLATE = require "speech_esctemplate"
 
 -- The character's name as appears in-game 
-STRINGS.NAMES.MXYR = "mxyr"
-STRINGS.SKIN_NAMES.mxyr_none = "mxyr"
+STRINGS.NAMES.ESCTEMPLATE = "Esc"
+STRINGS.SKIN_NAMES.esctemplate_none = "Esc"
 
 -- The skins shown in the cycle view window on the character select screen.
 -- A good place to see what you can put in here is in skinutils.lua, in the function GetSkinModes
@@ -67,4 +67,4 @@ local skin_modes = {
 }
 
 -- Add mod character to mod character list. Also specify a gender. Possible genders are MALE, FEMALE, ROBOT, NEUTRAL, and PLURAL.
-AddModCharacter("mxyr", "MALE", skin_modes)
+AddModCharacter("esctemplate", "FEMALE", skin_modes)
